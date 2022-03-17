9b7b56f6d2d008ae774ee9f292571ee01ee84b2e
martinsph <ph.mmartins@gmail.com>
2022-03-17 20:29:40 -0300

    add req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
 # Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    all_news = []
+    # Busca case-insensitive
+    # https://docs.mongodb.com/manual/reference/operator/query/regex/
+    # http://www.w3big.com/pt/mongodb/mongodb-regular-expression.html
+    result = search_news({'title': {'$regex': title, '$options': 'i'}})
+    for news in result:
+        all_news.append((news['title'], news['url']))
+
+    return all_news
 
 
 # Requisito 7
