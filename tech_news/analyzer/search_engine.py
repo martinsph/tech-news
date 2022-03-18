0a1ccbdf8d0e310879e22095c0f2f29cc10e6c45
martinsph <ph.mmartins@gmail.com>
2022-03-17 21:08:12 -0300

    add req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 # Requisito 6
+from datetime import datetime
 from tech_news.database import search_news
 
 
@@ -16,7 +17,18 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        all_news = []
+        # como usar strptime()
+        # https://www.educative.io/edpresso/how-to-convert-a-string-to-a-date-in-python
+        datetime.strptime(date, '%Y-%m-%d')
+        result = search_news({'timestamp': {'$regex': date}})
+        for news in result:
+            all_news.append((news['title'], news['url']))
+
+        return all_news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
